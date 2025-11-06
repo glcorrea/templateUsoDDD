c1102afeba526b274af0a7b4c984cb10aa1b4bcb
Gilson Luchesi Correa <correa_gilson@hotmail.com>
2025-11-06 08:22:42 -0300

    inclusão de um caso de teste customer

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -63,6 +63,17 @@ import Customer from "./customer";
             expect(customer.isActivate()).toBe(false);
         });
 
+        it ("should add reward points", () => {
+            const customer = new Customer("1", "Customer 1");
+            expect(customer.rewardPoints).toBe(0); 
+
+            customer.addRewardPoints(10);
+            expect(customer.rewardPoints).toBe(10);  
+            
+            customer.addRewardPoints(10);
+            expect(customer.rewardPoints).toBe(20);
+        })
+
 
 
 
