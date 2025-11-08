0dbb11c9a806a9417f6d12f88dd2aa24dd9f3509
Gilson Luchesi Correa <correa_gilson@hotmail.com>
2025-11-08 11:11:17 -0300

    implementação do Order e Ordem Item Repository

--- src/domain/entity/orderItem.ts
+++ src/domain/entity/orderItem.ts
@@ -1,21 +1,33 @@
 export default class OrderItem {
 
     private _id: string;
-    private _orderId: string;
+    private _productId: string;
     private _name: string;
     private _price: number;
     private _quantity: number
     
     
-    constructor(id: string, orderId:string, name: string, price: number, quantity: number) {
+    constructor(id: string, productId:string, name: string, price: number, quantity: number) {
         this._id = id;
-        this._orderId = orderId;
+        this._productId = productId;
         this._name = name;
         this._price = price;
         this._quantity = quantity;
     }
 
+    
+    get id(): string {
+        return this._id;
+    }
 
+    get productId(): string {
+        return this._productId;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+    
     get quantity(): number {
         return this._quantity;
     }
